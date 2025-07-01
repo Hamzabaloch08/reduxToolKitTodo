f90450fc69be41b148dea9df87100a7a652ce5b3
Hamzabaloch08 <hb.developer08@gmail.com>
2025-07-01 22:39:48 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import AddTodo from "./components/AddTodo"
-import ClearAllTodo from "./components/ClearAllTodo"
-import Todos from "./components/todos"
+import AddTodo from "./components/AddTodo";
+import ClearAllTodo from "./components/ClearAllTodo";
+import Todos from "./components/Todos";
 
 function App() {
-
   return (
     <div className="flex flex-col justify-between h-screen">
-    <AddTodo/>
-    <Todos/>
-    <ClearAllTodo/>
+      <AddTodo />
+      <Todos />
+      <ClearAllTodo />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
